912d9f6fda78a8a5660dd5d7725fb21fde8d9be0
coenjan <64894112+coenjan@users.noreply.github.com>
2023-03-14 15:08:03 +0100

    Add files via upload

---
+++ MPI_libs/Build_lmsmpi.bat
@@ -0,0 +1,10 @@
+:: Adapted from https://abhilashreddy.com/writing/3/mpi_instructions.html
+:: TODO: Install MPI and MPI SDK, run "set MSMPI" to check install and see the relevant directories %MSMPI_LIB64% and %MSMPI_INC% that should be added to PATH 
+copy "%MSMPI_LIB64%\msmpi.lib" %~dp0
+copy "%SYSTEMROOT%\System32\msmpi.dll" %~dp0
+gendef %~dp0msmpi.dll
+move .\msmpi.def %~dp0
+dlltool -d %~dp0msmpi.def -l %~dp0libmsmpi.a -D %~dp0msmpi.dll
+:: TODO: Change include "mpifptr.h" to "x64/mpifptr.h" in %MSMPI_INC%\mpi.90
+gfortran -fallow-invalid-boz "%MSMPI_INC%\mpi.f90"
+move .\mpi*.mod %~dp0
